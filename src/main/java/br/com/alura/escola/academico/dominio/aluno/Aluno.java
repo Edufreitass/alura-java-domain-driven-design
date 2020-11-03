bf6767c73f929d7ada5cf2b640d1073c5faa4538
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-03 16:50:31 -0300

    Contextos delimitados: Separando os contextos

---
+++ src/main/java/br/com/alura/escola/academico/dominio/aluno/Aluno.java
@@ -0,0 +1,58 @@
+package br.com.alura.escola.academico.dominio.aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// AGGREGATE ROOT
+public class Aluno {
+
+	private CPF cpf;
+	private String nome;
+
+	private Email email;
+
+	private List<Telefone> telefones = new ArrayList<>();
+
+	private String senha;
+
+	public Aluno(CPF cpf, String nome, Email email) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public void adicionarTelefone(String ddd, String numero) {
+		// INVARIANTE
+		/*
+		 * Se um aluno tiver mais do que 2 telefones em nosso sistema, essa regra foi
+		 * violada, logo, o Aluno estará em um estado inválido. Invariantes nada mais
+		 * são do que regras de negócio que precisam ser verificadas para garantir sua
+		 * consistência.
+		 */
+		if (telefones.size() == 2) {
+			throw new IllegalArgumentException("Numero maximo de telefones ja atingido!");
+		}
+		this.telefones.add(new Telefone(ddd, numero));
+	}
+
+	public CPF getCpf() {
+		return cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email.getEndereco();
+	}
+
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+}
---
+++ src/main/java/br/com/alura/escola/academico/dominio/aluno/LogDeAlunoMatriculado.java
@@ -0,0 +1,27 @@
+package br.com.alura.escola.academico.dominio.aluno;
+
+import java.time.format.DateTimeFormatter;
+
+import br.com.alura.escola.academico.dominio.Evento;
+import br.com.alura.escola.academico.dominio.Ouvinte;
+
+public class LogDeAlunoMatriculado extends Ouvinte {
+
+	public void reageAo(Evento evento) {
+		String momentoFormatado = evento
+				.momento()
+				.format(DateTimeFormatter
+						.ofPattern("dd/MM/yyyy HH:MM"));
+		System.out.println(String.format(
+				"Aluno cm CPF %s matriculado em: %s", 
+				((AlunoMatriculado) evento).getCpfDoAluno(), 
+				momentoFormatado));
+	}
+
+	// Só irá devolver true se o evento for de AlunoMatriculado
+	@Override
+	protected boolean deveProcessar(Evento evento) {
+		return evento instanceof AlunoMatriculado;
+	}
+	
+}
