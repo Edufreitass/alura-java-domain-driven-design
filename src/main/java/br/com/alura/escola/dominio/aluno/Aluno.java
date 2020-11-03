5537450b211ed21b74e400127bde571cdc720ca6
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-03 15:16:12 -0300

    Aggregates: Invariantes, protegendo o acesso e persistência de Aggregates

--- src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
@@ -3,17 +3,18 @@ package br.com.alura.escola.dominio.aluno;
 import java.util.ArrayList;
 import java.util.List;
 
+// AGGREGATE ROOT
 public class Aluno {
-	
+
 	private CPF cpf;
 	private String nome;
-	
+
 	private Email email;
-	
+
 	private List<Telefone> telefones = new ArrayList<>();
-	
+
 	private String senha;
-	
+
 	public Aluno(CPF cpf, String nome, Email email) {
 		this.cpf = cpf;
 		this.nome = nome;
@@ -21,24 +22,31 @@ public class Aluno {
 	}
 
 	public void adicionarTelefone(String ddd, String numero) {
+		// INVARIANTE
+		/*
+		 * Se um aluno tiver mais do que 2 telefones em nosso sistema, essa regra foi
+		 * violada, logo, o Aluno estará em um estado inválido. Invariantes nada mais
+		 * são do que regras de negócio que precisam ser verificadas para garantir sua
+		 * consistência.
+		 */
 		if (telefones.size() == 2) {
 			throw new IllegalArgumentException("Numero maximo de telefones ja atingido!");
 		}
 		this.telefones.add(new Telefone(ddd, numero));
 	}
-	
+
 	public String getCpf() {
 		return cpf.getNumero();
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public String getEmail() {
 		return email.getEndereco();
 	}
-	
+
 	public List<Telefone> getTelefones() {
 		return telefones;
 	}
@@ -46,11 +54,5 @@ public class Aluno {
 	public String getSenha() {
 		return senha;
 	}
-	
+
 }
-
-
-
-
-
-
